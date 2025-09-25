4d11a5b3b215361585eb0866dc4e73eeac7afa22
Rahmatillo <Rahmatillo@gmail.com>
2025-09-25 16:10:33 +0600

    cors error

--- SweetCraft/settings.py
+++ SweetCraft/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -111,8 +114,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://sweetcraft.onrender.com",
+]
+
 
 AUTH_USER_MODEL = 'cake.CustomUser'
 
